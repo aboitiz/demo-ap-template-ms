ceae7c8a5abed9abb235b4be8c8e0487f2198e39
carlo-castro <carlo.castro@novare.com.hk>
2023-01-26 10:27:50 +0800

    Initial commit for configuration per environment

--- src/main/java/com/apc/template/InquiryServiceApp.java
+++ src/main/java/com/apc/template/InquiryServiceApp.java
@@ -1,19 +1,30 @@
 package com.apc.template;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @EnableSwagger2
 @SpringBootApplication
+@Slf4j
 public class InquiryServiceApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InquiryServiceApp.class, args);
-		System.out.print(System.currentTimeMillis());
+    @Value("${environment.level}")
+    private String env;
 
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InquiryServiceApp.class, args);
+    }
+
+    @Bean
+    CommandLineRunner run() {
+        return args -> {
+            log.info("Environment: " + env);
+        };
+    }
 }
