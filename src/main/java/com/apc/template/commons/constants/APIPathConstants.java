e9845d1984a511ba4d72938f2794fdb180019c50
Initial Commit <jeremy.sasis@aboitiz.com>
2023-02-06 11:29:17 +0800

    removed private class in APIConstantsPath

--- src/main/java/com/apc/template/commons/constants/APIPathConstants.java
+++ src/main/java/com/apc/template/commons/constants/APIPathConstants.java
@@ -1,10 +1,6 @@
 package com.apc.template.commons.constants;
 
 public class APIPathConstants {
-
-    private APIPathConstants() {
-    }
-
     public static final String USER_BASE_PATH = "/user";
     public static final String API_VERSION_1 = "/v1/api";
     public static final String CUSTOMER_BASE_PATH = "/customer";
